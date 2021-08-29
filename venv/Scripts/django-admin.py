1ad470ca32a1a4dcdde2a5c32bfa03039c0aa1d3
tomaschild <tomas.child@usach.cl>
2021-08-29 19:22:33 -0400

    Se agrega venv y se modifica gitignore

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\enecr\documents\repos\buendia\cotizaciones-buendia\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

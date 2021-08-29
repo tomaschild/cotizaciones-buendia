16746f7c4204ddfb357500b32c236d382f25db40
Chacon <63523896+Chacon-tu-drema@users.noreply.github.com>
2021-08-30 00:31:01 +0100

    borrar la maquina

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\enecr\documents\repos\buendia\cotizaciones-buendia\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
